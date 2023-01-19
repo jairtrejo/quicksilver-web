cb99ae982813453f18fe34719d3f3f2e2acb1395
Jair Trejo <jair@jairtrejo.mx>
2023-01-18 17:04:02 -0800

    MVP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useSWRMutation from "swr/mutation";
 
 function cx(classnames) {
   return classnames.filter((e) => e).join(" ");
+}
+
+async function postRequest(url, { arg }) {
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  let response;
+  try {
+    response = await fetch(baseUrl + url, {
+      method: "POST",
+      body: JSON.stringify(arg),
+    });
+  } catch {
+    throw new Error("Backend is not responsive.");
+  }
+
+  if (!response.ok) {
+    let message;
+    try {
+      const responseData = await response.json();
+      message = responseData.message;
+    } catch {
+      message = "Backend is not responsive.";
+    }
+    throw new Error(message);
+  } else {
+    return await response.json();
+  }
 }
 
 function App() {
@@ -25,6 +53,21 @@ function App() {
   ];
 
   const [active, setActive] = useState(new Set());
+  const suggestionInput = useRef(null);
+  const {
+    data = {id: ""},
+    trigger: suggestPrompt,
+    isMutating,
+    error,
+  } = useSWRMutation("/prompt", postRequest, { throwOnError: false });
+
+  function submitSuggestion(e) {
+    const prompt = suggestionInput.current.value;
+
+    suggestPrompt({ prompt });
+
+    e.preventDefault();
+  }
 
   return (
     <>
@@ -34,14 +77,26 @@ function App() {
           Suggest a Stable Diffusion prompt to generate me an avatar. Refer to
           me as jairtrejo (one word).
         </p>
-        <form action="">
+        <form action="" onSubmit={submitSuggestion}>
           <input
             id="prompt"
+            key={data.id}
+            ref={suggestionInput}
             type="text"
             name="prompt"
             placeholder="An oil portrait of jairtrejo in the style of Rembrandt"
           />
-          <button type="submit">Suggest</button>
+          <button disabled={isMutating} type="submit">
+            {isMutating ? "Sending..." : "Suggest"}
+          </button>
+          <p className="error">
+            {!isMutating && error ? (
+              <>
+                <strong>Error:</strong> {error.message}
+              </>
+            ) : null}
+            {data && !isMutating && !error ? 'Thank you for your suggestion!' : null}
+          </p>
         </form>
         <p>
           One will be picked at random every day an set as{" "}
